e71277354a99820c42b39972ded2b7736078ab22
emmettlucas <twiddlemaster@gmail.com>
2016-10-17 09:16:12 -0700

    Added all current files from the test program

---
+++ test/core/src/com/mygdx/game/Drop.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+/**
+ * Created by emmet on 10/7/2016.
+ */
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public class Drop extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    public void create() {
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render();
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
---
+++ test/core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,59 @@
+package com.mygdx.game;
+
+/**
+ * Created by emmet on 10/7/2016.
+ */
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
+
+    final Drop game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Drop gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
