cbcce7739c2718b4d14586a3a2658adb3cecc927
emmettlucas <twiddlemaster@gmail.com>
2016-11-18 09:37:08 -0800

    Added bricks

--- CollegeApp/core/src/com/mygdx/game/CollegeApp.java
+++ CollegeApp/core/src/com/mygdx/game/CollegeApp.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.objects.Brick;
+import com.mygdx.game.objects.GameObject;
+
+import java.util.ArrayList;
 
 public class CollegeApp extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private PixelGuy player1;
+	private com.mygdx.game.objects.PixelGuy player1;
+	private ArrayList<GameObject> list = new ArrayList<GameObject>();
 	
 	@Override
 	public void create () {
@@ -21,8 +24,14 @@ public class CollegeApp extends ApplicationAdapter {
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
 
-		player1 = new PixelGuy();
+		player1 = new com.mygdx.game.objects.PixelGuy();
 		player1.setPosition(200, 100);
+
+		list.add(new Brick(0,0));
+		list.add(new Brick(64,0));
+		list.add(new Brick(128,0));
+		list.add(new Brick(256,128));
+		list.add(new Brick(320,128));
 	}
 
 	@Override
@@ -33,6 +42,9 @@ public class CollegeApp extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		player1.draw(batch);
+		for(GameObject t: list){
+			t.draw(batch);
+		}
 		batch.end();
 
 		//Updates
@@ -40,6 +52,12 @@ public class CollegeApp extends ApplicationAdapter {
 		Rectangle temp = new Rectangle(0,0,800,20);
 		if (player1.hits(temp) != -1){
 			player1.action(1, 0, 20);
+		}
+
+		for(GameObject t: list){
+			if(player1.hits(t.getHitBox()) != -1){
+				player1.action(1, 0, t.getHitBox().y + t.getHitBox().height);
+			}
 		}
 
 		//Controls
